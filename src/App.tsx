71821b9cc82f998115ea432dc285a4a3ed52f5be
informaticscorporation <corporationinformatics@gmail.com>
2024-02-15 12:52:23 +0100

    implementazione card, html, css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from 'react-dom';
+
+interface Product {
+  id: number;
+  thumbnail:string;
+  title: string;
+  price: number;
+  description: string;
+}
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+ 
+
+  useEffect(() => {
+    async function getCard() {
+      try {
+        const response = await fetch("https://mockend.up.railway.app/api/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+
+    
+    getCard();
+  }, []); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div id='container'>
+    <div id='nav'>
+    <img id='img' src={require('./png-clipart-computer-icons-shopping-cart-shopping-cart-text-shopping-centre.png')} alt="" />
+      <h1>Shop</h1>
     </div>
+    <div id='container-card'>
+
+      {products.map((product) => (
+        <div key={product.id} className="card">
+          <img src={product.thumbnail} alt="" />
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+          <button>Aggiungi al carrello</button>
+        </div>
+      ))}
+     </div>
+  </div>
   );
 }
 
